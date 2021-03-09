57567b5b2b64665d18b509249e44a8676aef3844
bateso88 <bateso@hotmail.co.uk>
2021-03-09 18:13:23 +0000

    refactor regular_item class

--- src/regular_item.js
+++ src/regular_item.js
@@ -3,13 +3,11 @@ const {Item} = require("../src/item");
 class RegularItem extends Item {
   updateQuality() {
     if(this.sellIn <= 0) {
-      for(let i=0; i<2; i++) {
-        this.quality--          //reduce by 2 if past sell by date
-      }
+      this.quality -= 2
     } else {
       this.quality--            //reduce by 1 if not past sell by date
     }
-    if(this.quality < 0 ) {
+    if(this.quality < 0) {
       this.quality = 0          //set quality to zero if currently neagtive
     }
     this.sellIn--;              // reduce sellIn
