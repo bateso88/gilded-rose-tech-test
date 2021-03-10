da116b3f162b7ed3bd0f82b85bb8742af2497bdb
bateso88 <bateso@hotmail.co.uk>
2021-03-10 13:54:53 +0000

    remove magic numbers from aged_brie and backstage_pass

--- src/aged_brie.js
+++ src/aged_brie.js
@@ -1,8 +1,9 @@
 const {Item} = require("../src/item");
+const MAX_QUALITY = 50;
 
 class AgedBrie extends Item {
   updateQuality() {
-    if(this.quality < 50) {
+    if(this.quality < MAX_QUALITY) {
       this.quality++            //increase by 1 if under 50
     } 
     this.sellIn--;              // reduce sellIn
--- src/backstage_pass.js
+++ src/backstage_pass.js
@@ -1,16 +1,19 @@
 const {Item} = require("../src/item");
+const MAX_QUALITY = 50;
+const TIER_TWO_UPPER_LIMIT = 10;
+const TIER_THREE_UPPER_LIMIT = 5;
 
 class BackstagePass extends Item {
   updateQuality() {
     this.quality++              // Increase quality by 1
-    if(this.sellIn <= 10) {
+    if(this.sellIn <= TIER_TWO_UPPER_LIMIT) {
       this.quality++            // Increase quality by 2 if sellIn <= 10
-      if(this.sellIn <= 5) {
+      if(this.sellIn <= TIER_THREE_UPPER_LIMIT) {
         this.quality++          // Increase quality by 3 if sellIn <= 5
       }
     }
-    if(this.quality > 50) {
-      this.quality = 50         // Set to max quality if appropriate
+    if(this.quality > MAX_QUALITY) {
+      this.quality = MAX_QUALITY // Set to max quality if appropriate
     }
     if(this.sellIn < 0) {
       this.quality = 0          // Set to 0 if sellIn < 0 
