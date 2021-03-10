e7267b3820337f7c4c3e1bc16c0f23e8eca27a70
bateso88 <bateso@hotmail.co.uk>
2021-03-10 14:30:25 +0000

    refactor backstage_pass

--- src/backstage_pass.js
+++ src/backstage_pass.js
@@ -7,19 +7,31 @@ class BackstagePass extends Item {
   updateQuality() {
     this.sellIn--               // reduce sellIn
     this.quality++              // Increase quality by 1
+    this._increaseQualityIfTierTwoOrThree()
+    this._setQualityToMaxIfNecessary()
+    this._setQualityToZeroIfSellInHasPassed()
+  } 
+  _increaseQualityIfTierTwoOrThree() {
     if(this.sellIn <= TIER_TWO_UPPER_LIMIT) {
-      this.quality++            // Increase quality by 2 if sellIn <= 10
-      if(this.sellIn <= TIER_THREE_UPPER_LIMIT) {
-        this.quality++          // Increase quality by 3 if sellIn <= 5
-      }
+      this.quality++            
+      this._increaseQualityIfTierThree()
     }
+  }  
+  _increaseQualityIfTierThree() {
+    if(this.sellIn <= TIER_THREE_UPPER_LIMIT) {
+      this.quality++         
+    }
+  }
+  _setQualityToMaxIfNecessary() {
     if(this.quality > MAX_QUALITY) {
       this.quality = MAX_QUALITY // Set to max quality if appropriate
     }
+  } 
+  _setQualityToZeroIfSellInHasPassed() {
     if(this.sellIn < 0) {
       this.quality = 0          // Set to 0 if sellIn < 0 
     }
-  }           
+  }       
 }
 
 module.exports = {
