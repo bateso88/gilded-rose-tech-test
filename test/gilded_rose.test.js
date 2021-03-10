8fa3e54a0b780ac27430096cdd0650207da71df5
bateso88 <bateso@hotmail.co.uk>
2021-03-10 12:14:48 +0000

    refactor shop class and add relevant feature testing

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -5,26 +5,40 @@ const {Sulfuras} = require("../src/sulfuras");
 const {BackstagePass} = require("../src/backstage_pass");
 
 describe("Gilded Rose", function() {
-  const gildedRose = new Shop([new RegularItem("Thing", 10, 10), 
+  const gildedRose = new Shop([new RegularItem("Thing", 10, 10),
+  new RegularItem("Thing2", -1, 10),
   new AgedBrie("Brie", 10, 10),
   new Sulfuras("Sulfuras", 10, 10),
-  new BackstagePass("Gig", 10, 10)]
+  new BackstagePass("Gig1", 11, 10),
+  new BackstagePass("Gig2", 10, 10),
+  new BackstagePass("Gig3", 5, 10),
+  new BackstagePass("Gig4", -1, 10),]
   );
 
-  it("RegularItem deprecates", function() {
-    const items = gildedRose.updateQuality();
+  const items = gildedRose.updateQuality();
+
+  it("RegularItem quality decreases by one", function() {
     expect(items[0].quality).toEqual(9);
   });
-  // it("AgedBrie improves", function() {
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[1].quality).toEqual(11);
-  // });
-  // it("Sulfuras remain the same", function() {
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[2].quality).toEqual(10);
-  // });
-  // it("BackstagePass improves", function() {
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[3].quality).toEqual(11);
-  // });
+  it("RegularItem quality decreases twice after sellIn", function() {
+    expect(items[1].quality).toEqual(8);
+  });
+  it("AgedBrie improves", function() {
+    expect(items[2].quality).toEqual(11);
+  });
+  it("Sulfuras remain the same", function() {
+    expect(items[3].quality).toEqual(10);
+  });
+  it("BackstagePass quality increases by 1 when sellIn > 10", function() {
+    expect(items[4].quality).toEqual(11);
+  });
+  it("BackstagePass quality increases by 2 when sellIn <= 10", function() {
+    expect(items[5].quality).toEqual(12);
+  });
+  it("BackstagePass quality increases by 3 when sellIn <= 5", function() {
+    expect(items[6].quality).toEqual(13);
+  });
+  it("BackstagePass quality is 0 sellIn <0", function() {
+    expect(items[7].quality).toEqual(0);
+  });
 });
